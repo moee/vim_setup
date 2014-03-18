1af4e3b23322e3ca3d069a22e550151842b0765d
moee <moee@github>
2014-03-18 10:40:44 +0100

    Add some colors

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Attention: This file will overwrite your vim settings without asking.
 # You have been warned.
 
+success () { echo -e "\e[00;32mSuccess\e[00m"; }
+error () { echo -e "\e[00;31mError\e[00m"; }
+info () { echo -e "\e[00;34m$1\e[00m"; }
+
 if [ -z $HOME ]; then
     echo "\$HOME does not exist"
     exit 1 
@@ -21,30 +25,30 @@ if [ ! -d $PLUGINDIR ]; then
     exit 1
 fi
 
-echo installing mru plugin
+info "installing mru plugin"
 
 if [ -d mru.vim ]; then
     rm mru.vim -rf
 fi
 
-(git clone https://github.com/vim-scripts/mru.vim && cp mru.vim/plugin/mru.vim $PLUGINDIR/mru.vim && echo "success") || echo "failed"
+(git clone https://github.com/vim-scripts/mru.vim && cp mru.vim/plugin/mru.vim $PLUGINDIR/mru.vim && success) || error
 
-echo installing pathogem.vim
+info "installing pathogem.vim"
 mkdir -p ~/.vim/autoload ~/.vim/bundle 
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-echo installing SnipMate
+info "installing SnipMate"
 cd ~/.vim/bundle
 git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/vim-snippets.git
 
-echo installing custom snippets
-cp $SCRIPTPATH/bundle/mysnippets $HOME/.vim/bundle -R && echo "success"
+info "installing custom snippets"
+cp $SCRIPTPATH/bundle/mysnippets $HOME/.vim/bundle -R && success
 
-echo installing filetype.vim
-cp $SCRIPTPATH/filetype.vim $HOME/.vim/filetype.vim && echo "success"
+info "installing filetype.vim"
+cp $SCRIPTPATH/filetype.vim $HOME/.vim/filetype.vim && success
 
-echo installing .vimrc
-cp $SCRIPTPATH/.vimrc $HOME/.vimrc && echo "success"
+info "installing .vimrc"
+cp $SCRIPTPATH/.vimrc $HOME/.vimrc && success
